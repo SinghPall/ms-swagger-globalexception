de2314974aa26ab911b4038fe81e81df030fd3ba
SinghPall <pallavisinghsep@gmail.com>
2020-07-09 13:26:47 +0530

    Added test class

--- service3/src/test/java/com/ms/assignmnet/service3/Service3ApplicationTests.java
+++ service3/src/test/java/com/ms/assignmnet/service3/Service3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Service21ApplicationTests {
+class Service3ApplicationTests {
 
 	@Test
 	void contextLoads() {
